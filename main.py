21f445601e444200bdcbcec49f292fa27ec670d0
vanashimko <vana.shimko@gmail.com>
2017-03-29 10:39:20 +0300

    Add random distance matrix generating

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import argparse
+import numpy as np
 from hierarchical_clustering import get_linkage_matrix
 from drawer import draw_result
 
-DISTANCE_MATRIX = [
+
+BOOK_EXAMPLE = [
     [0., 5., 0.5, 2.],
     [5., 0., 1., 0.6],
     [0.5, 1., 0., 2.5],
@@ -10,7 +13,17 @@ DISTANCE_MATRIX = [
 
 
 def main():
-    linkage_matrix = get_linkage_matrix(DISTANCE_MATRIX)
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--r', dest='random_size', type=int, help='generated matrix size')
+    args = argparser.parse_args()
+
+    if args.random_size is not None:
+        matrix = np.random.rand(args.random_size, args.random_size)
+        matrix = (matrix + matrix.T)/2
+    else:
+        matrix = BOOK_EXAMPLE
+
+    linkage_matrix = get_linkage_matrix(matrix)
     draw_result(linkage_matrix)
 
 
